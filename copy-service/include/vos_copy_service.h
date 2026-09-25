9a496643abda8ceb57ed06235bc814b43cfc5112
Edward Miller <symbiogenesis@outlook.com>
2026-09-25 00:46:28 -0500

    Use the owned C comment syntax for SPDX headers

--- copy-service/include/vos_copy_service.h
+++ copy-service/include/vos_copy_service.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+// SPDX-License-Identifier: Apache-2.0
 #ifndef VOS_COPY_SERVICE_H
 #define VOS_COPY_SERVICE_H
 
--- copy-service/test/host.c
+++ copy-service/test/host.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+// SPDX-License-Identifier: Apache-2.0
 #include "vos_copy_service.h"
 #include <errno.h>
 #include <stdio.h>
